a02b4b4b9cdf46ff0c1e9e7dc9c5e381395586de
ddc.05 <chinh000529@gmail.com>
2021-12-06 11:59:31 +0700

    fix

--- index.js
+++ index.js
@@ -2,27 +2,54 @@ const XLSX = require("xlsx");
 
 // convert file xlsx to array
 const wb = XLSX.readFile('master.xlsx');
-const ws = wb.SheetNames[0];
-const data = XLSX.utils.sheet_to_json(wb.Sheets[ws]);
-const result = data.map(item => {
+const ws = wb.Sheets[wb.SheetNames[0]];
+const result = [];
+let item = {};
+for (let cell in ws) {
+    const cellAsString = cell.toString();
+    if (cellAsString[1] !== 'r' && cellAsString !== 'm' && cellAsString[1] > 1) {
+        if (cellAsString[0] === 'A') {
+            item.id = ws[cell].v;
+        }
+        if (cellAsString[0] === 'B') {
+            item.no = ws[cell].v;
+        }
+        if (cellAsString[0] === 'P') {
+            item.a = ws[cell].v;
+        }
+        if (cellAsString[0] === 'Q') {
+            item.b = ws[cell].v;
+        }
+        if (cellAsString[0] === 'R') {
+            item.c = ws[cell].v;
+        }
+        if (cellAsString[0] === 'S') {
+            item.d = ws[cell].v;
+            result.push(item);
+            item = {};
+        }
+    }
+}
+const newResult = result.map(item => {
     return Object.values(item);
 })
-console.log(result);
+console.log(newResult);
 
 
-// convert file json to xlsx
-const newWB = XLSX.utils.book_new();
-const newWS = XLSX.utils.json_to_sheet([
-    {
-        "id": "#2905",
-        "name": "Chinh",
-        "age": 21
-    },
-    {
-        "id": "#2807",
-        "name": "Nga",
-        "age": 21
-    },
-]);
-XLSX.utils.book_append_sheet(newWB, newWS, "information");
-XLSX.writeFile(newWB, 'information.xlsx');
+// // convert file json to xlsx
+// const newWB = XLSX.utils.book_new();
+// const newWS = XLSX.utils.json_to_sheet([
+//     {
+//         "id": "#2905",
+//         "name": "Chinh",
+//         "age": 21
+//     },
+//     {
+//         "id": "#2807",
+//         "name": "Nga",
+//         "age": 21
+//     },
+// ]);
+// XLSX.utils.book_append_sheet(newWB, newWS, "information");
+// XLSX.writeFile(newWB, 'information.xlsx');
+
